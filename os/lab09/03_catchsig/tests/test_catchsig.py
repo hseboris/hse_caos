319b1a74b40d0cee222eb69742cb52feaf3750ac
Boris V. Galitskii <galitskiiboris@mail.ru>
2025-05-23 17:11:46 +0300

    new tests

--- os/lab09/03_catchsig/tests/test_catchsig.py
+++ os/lab09/03_catchsig/tests/test_catchsig.py
@@ -26,13 +26,13 @@ class TestCatchSig(unittest.TestCase):
         out = self.run_and_kill("INT", signal.SIGINT)
         self.assertRegex(out, r"\[Caught: .*Interrupt.*\]")
 
-    def test_catches_term(self):
-        out = self.run_and_kill("TERM", signal.SIGTERM)
-        self.assertRegex(out, r"\[Caught: .*Term.*\]")
+    def test_catches_usr1(self):
+        out = self.run_and_kill("USR1", signal.SIGUSR1)
+        self.assertRegex(out, r"\[Caught: .*User.*\]")
 
-    def test_catches_pipe(self):
-        out = self.run_and_kill("PIPE", signal.SIGPIPE)
-        self.assertRegex(out, r"\[Caught: .*Pipe.*\]")
+    def test_catches_alrm(self):
+        out = self.run_and_kill("ALRM", signal.SIGALRM)
+        self.assertRegex(out, r"\[Caught: .*Alarm.*\]")
 
     def test_invalid_signal(self):
         result = subprocess.run(
@@ -47,24 +47,24 @@ class TestCatchSig(unittest.TestCase):
 
     def test_multiple_signals(self):
         proc = subprocess.Popen(
-            [self.binary, "1", "INT", "TERM", "PIPE"],
+            [self.binary, "1", "INT", "USR1", "ALRM"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
         time.sleep(2)
-        os.kill(proc.pid, signal.SIGPIPE)
+        os.kill(proc.pid, signal.SIGUSR1)
         time.sleep(2)
-        os.kill(proc.pid, signal.SIGTERM)
+        os.kill(proc.pid, signal.SIGALRM)
         time.sleep(2)
         os.kill(proc.pid, signal.SIGINT)
         time.sleep(1)
         proc.terminate()
         out, _ = proc.communicate()
 
-        self.assertRegex(out, r"\[Caught: .*Pipe.*\]")
-        self.assertRegex(out, r"\[Caught: .*Term.*\]")
+        self.assertRegex(out, r"\[Caught: .*User.*\]")
         self.assertRegex(out, r"\[Caught: .*Interrupt.*\]")
+        self.assertRegex(out, r"\[Caught: .*Alarm.*\]")
 
 if __name__ == '__main__':
     unittest.main()
