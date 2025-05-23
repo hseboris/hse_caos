020b50609cfb7f833cb9abe6b59040c44ca938ed
Boris V. Galitskii <galitskiiboris@mail.ru>
2025-05-23 15:49:13 +0300

    fixed

--- os/lab09/03_catchsig/tests/test_catchsig.py
+++ os/lab09/03_catchsig/tests/test_catchsig.py
@@ -8,37 +8,61 @@ class TestCatchSig(unittest.TestCase):
     def setUp(self):
         self.binary = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'catchsig'))
 
-    def test_catches_sigint(self):
+    def run_and_kill(self, signame, sig):
         proc = subprocess.Popen(
-            [self.binary, "1", "INT"],
+            [self.binary, "1", signame],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
-
         time.sleep(1.5)
-        os.kill(proc.pid, signal.SIGINT)
+        os.kill(proc.pid, sig)
         time.sleep(1)
         proc.terminate()
-        out, _ = proc.communicate()
+        out, err = proc.communicate(timeout=2)
+        return out
 
-        self.assertIn("[Caught: Interrupt]", out)
+    def test_catches_sigint(self):
+        out = self.run_and_kill("INT", signal.SIGINT)
+        self.assertRegex(out, r"\[Caught: .*Interrupt.*\]")
 
     def test_catches_abrt(self):
-        proc = subprocess.Popen(
-            [self.binary, "1", "ABRT"],
+        out = self.run_and_kill("ABRT", signal.SIGABRT)
+        self.assertRegex(out, r"\[Caught: .*Abort.*\]")
+
+    def test_catches_usr1(self):
+        out = self.run_and_kill("USR1", signal.SIGUSR1)
+        self.assertRegex(out, r"\[Caught: .*User.*\]")
+
+    def test_ignores_invalid_signal(self):
+        # запускаем с неверным сигналом
+        result = subprocess.run(
+            [self.binary, "1", "FAKESIGNAL"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
         )
+        self.assertIn("Cannot handle signal", result.stderr)
 
-        time.sleep(1.5)
+    def test_multiple_signals(self):
+        proc = subprocess.Popen(
+            [self.binary, "1", "INT", "ABRT", "USR1"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        time.sleep(1)
+        os.kill(proc.pid, signal.SIGUSR1)
+        time.sleep(1)
+        os.kill(proc.pid, signal.SIGINT)
+        time.sleep(1)
         os.kill(proc.pid, signal.SIGABRT)
         time.sleep(1)
         proc.terminate()
         out, _ = proc.communicate()
-
-        self.assertIn("[Caught: Aborted]", out)
+        self.assertRegex(out, r"\[Caught: .*Interrupt.*\]")
+        self.assertRegex(out, r"\[Caught: .*Abort.*\]")
+        self.assertRegex(out, r"\[Caught: .*User.*\]")
 
 if __name__ == '__main__':
     unittest.main()
