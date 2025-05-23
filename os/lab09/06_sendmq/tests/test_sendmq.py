5440109c19b69066833fa1f00ca23072bac2883d
Boris V. Galitskii <galitskiiboris@mail.ru>
2025-05-23 23:46:17 +0300

    fixed

--- os/lab09/06_sendmq/tests/test_sendmq.py
+++ os/lab09/06_sendmq/tests/test_sendmq.py
@@ -5,24 +5,35 @@ import time
 
 class TestSendMQ(unittest.TestCase):
     def setUp(self):
-        self.bin = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'sendmq'))
+        self.sendmq = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'sendmq'))
         self.queue = "/testmq_send"
-        self.message = "Test Message"
+        self.message = "Hello, MQ!"
 
-        # Создать очередь
-        subprocess.run([
-            "mq_open", self.queue, "O_CREAT|O_RDWR", "0600", "10", "1024"
-        ], stderr=subprocess.DEVNULL)
+        # компилируем и запускаем crt_mq.c из лекции
+        with open("crt_mq_tmp.c", "w") as f:
+            f.write("""
+#include <mqueue.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main() {
+    struct mq_attr attr = {0};
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = 1024;
+    mq_open(\"/testmq_send\", O_CREAT | O_RDWR, 0600, &attr);
+    return 0;
+}
+""")
+        subprocess.run(["gcc", "crt_mq_tmp.c", "-lrt", "-o", "crt_mq_tmp"], check=True)
+        subprocess.run(["./crt_mq_tmp"], check=True)
 
     def tearDown(self):
-        try:
-            subprocess.run(["mq_unlink", self.queue], stderr=subprocess.DEVNULL)
-        except:
-            pass
+        subprocess.run(["rm", "-f", "crt_mq_tmp", "crt_mq_tmp.c"])
+        subprocess.run(["mq_unlink", self.queue], stderr=subprocess.DEVNULL)
 
     def test_send_message_success(self):
         result = subprocess.run(
-            [self.bin, self.queue, self.message],
+            [self.sendmq, self.queue, self.message],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
@@ -32,7 +43,7 @@ class TestSendMQ(unittest.TestCase):
 
     def test_send_invalid_queue(self):
         result = subprocess.run(
-            [self.bin, "/doesnotexist", self.message],
+            [self.sendmq, "/doesnotexist", self.message],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
