ab7e50fc91663469ff9eea6d2ec01ed1b0b18992
Boris V. Galitskii <galitskiiboris@mail.ru>
2025-05-26 20:17:51 +0300

    fixed

--- os/lab07/02_sumsum/tests/test_sumsum.py
+++ os/lab07/02_sumsum/tests/test_sumsum.py
@@ -18,24 +18,24 @@ class TestSumSum(unittest.TestCase):
         self.assertEqual(result.returncode, 0)
         self.assertEqual(result.stdout.strip(), expected)
 
-    def test_not_equal(self):
+    def test_case_1(self):
         self.run_case("1 3 5\n2 4 6\n", "Not equal")
 
-    def test_equal(self):
+    def test_case_2(self):
         self.run_case("1 5 6\n4 4 4\n", "Equal")
 
-    def test_both_invalid(self):
+    def test_case_3(self):
         self.run_case("1 2 w\n3 4 e\n", "Equal")
 
-    def test_one_invalid(self):
+    def test_case_4(self):
         self.run_case("1 2 1\n2 2 Q\n", "Not equal")
 
-    def test_negative_equal(self):
+    def test_case_5(self):
         self.run_case("qwe 3 4\n10 20 -30\n", "Equal")
 
     def test_large_input_with_error(self):
         line1 = " ".join(str(i) for i in range(100))        # сумма 0..99 = 4950
-        line2 = " ".join(str(i) for i in range(98)) + " X Y" # сумма 0..97 = 4753 → ошибка → 0
+        line2 = " ".join(str(i) for i in range(98)) + " X Y" # ошибка → 0
         self.run_case(f"{line1}\n{line2}\n", "Not equal")
 
 if __name__ == '__main__':
